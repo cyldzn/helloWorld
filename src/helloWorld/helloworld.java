b22ad8b432d4518142a1c1227850ccc71cc5f5c0
cyldzn <cn.yldz@hotmail.com>
2022-07-08 17:17:23 +0100

    hello java

---
+++ src/helloWorld/helloworld.java
@@ -0,0 +1,11 @@
+package helloWorld;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String greet = "Hello World";
+System.out.println(greet);
+	}
+
+}
